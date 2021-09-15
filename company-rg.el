12919de692f3e44b4442478ea117fed5d2da6eaf
Aaron Jensen <aaronjensen@gmail.com>
2021-09-15 08:39:05 -0400

    Header

--- company-rg.el
+++ company-rg.el
@@ -1,5 +1,33 @@
 ;;; company-rg.el --- rg backend for company-mode  -*- lexical-binding: t -*-
 
+;; Copyright (C) 2020-2021 Aaron Jensen
+
+;; Author: Aaron Jensen
+;; URL: https://github.com/aaronjensen/company-rg
+;;
+;; Version: 1.0
+;; Keywords: company
+;; Package-Requires: ((emacs "27.1") (company "0.9.0") (dash "2.2.0"))
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see
+;; <https://www.gnu.org/licenses/>.
+
+;; This file is not part of Emacs.
+
+;;; Commentary:
+;; A Company backend that uses rg.
+
 ;;; Code:
 
 (eval-when-compile
